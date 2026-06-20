51da30987c7e8861d7d017bb033219e3b6e02378
edroguajardo19928547yes <pedro_guajardo_1992@hotmail.com>
2026-06-20 10:54:27 -0300

    Added 20260620135359_remove_duplicate_pillars.sql

---
+++ supabase/migrations/20260620135359_remove_duplicate_pillars.sql
@@ -0,0 +1,6 @@
+DELETE FROM public.methodology_pillars WHERE id IN (
+  '16c8be79-c46e-4554-96a1-e2c8568953b5',
+  '29fd4e74-1245-44aa-bb4b-c851b6b27446',
+  'b23f33c9-1532-49a6-95eb-0af5f80d352e',
+  'cecb1084-bea6-4021-aa46-ec845af4c717'
+);
